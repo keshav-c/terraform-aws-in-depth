d7092c345da83c91beb5fee95d401ba74cec046e
Keshav Chakravarthy <contact@keccak.dev>
2025-07-14 12:30:20 +0530

    Convert this terraform config into a reusable module on github

--- lookups.tf
+++ lookups.tf
@@ -1,14 +1,3 @@
-data "aws_vpc" "default" {
-  default = true
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
 data "aws_ami" "ubuntu" {
   owners      = ["099720109477"]
   most_recent = true
